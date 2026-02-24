cfb8d22dceaa7418214b7301e5057615d0fd02ca
b-design-dk <brian.h.jorgensen@gmail.com>
2026-02-24 07:39:51 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,44 +1,45 @@
-const CACHE_NAME = "layers-v1.47.1";
+const CACHE_NAME = "layers-core-v0.1";
 
-const ASSETS = [
-	"/",
-	"/index.html",
-	"/admin.html",
-	"/manifest.json",
-
-	// styles
-	"/style/style.css",
-
-	// scripts
-	"/js/bootstrap.js",
-	"/js/crypto.js",
-	"/js/vault.js",
-	"/js/admin.js",
-	"/js/impact.js"
+const CORE_ASSETS = [
+  "/",
+  "/index.html",
+  "/admin.html",
+  "/style/style.css",
+  "/js/bootstrap.js",
+  "/js/crypto.js",
+  "/js/vault.js",
+  "/js/admin.js",
+  "/js/impact.js"
 ];
 
 self.addEventListener("install", event => {
-	event.waitUntil(
-		caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
-	);
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(CORE_ASSETS))
+  );
 });
 
 self.addEventListener("fetch", event => {
-	event.respondWith(
-		caches.match(event.request).then(response =>
-		response || fetch(event.request)
-		)
-	);
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+        const responseClone = response.clone();
+        caches.open(CACHE_NAME).then(cache => {
+          cache.put(event.request, responseClone);
+        });
+        return response;
+      })
+      .catch(() => caches.match(event.request))
+  );
 });
 
 self.addEventListener("activate", event => {
-	event.waitUntil(
-		caches.keys().then(keys =>
-			Promise.all(
-				keys
-					.filter(key => key !== CACHE_NAME)
-					.map(key => caches.delete(key))
-			)
-		)
-	);
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      )
+    )
+  );
 });
